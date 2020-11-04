e82c81decc6f8e9e10fab9349c654bbed411f124
yashsinghal98 <68867021+yashsinghal98@users.noreply.github.com>
2020-11-05 02:17:15 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,11 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <></>
+    <>
+    <div id="root">
+    <p>I am learning React. My life is getting better.</p>
+    </div>
+    </>
   )
 }
 
